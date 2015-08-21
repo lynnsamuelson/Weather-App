96c470e8aed355354d88c50f7da0bdfa75e71604
Dan-Hodges <dhdgs690@gmail.com>
2015-08-21 13:23:50 -0500

    added click funtion in main.js

--- javascripts/main.js
+++ javascripts/main.js
@@ -10,7 +10,18 @@ requirejs.config({
     "bootstrap": ["jquery"]  }
 });
 
-requirejs(["dependencies"], 
-  function(dependencies) {
+requirejs(["dependencies", "user-input"], 
+  function(dependencies, userInput) {
+  console.log("userInput.getZip() :", userInput.getZip());
 
+  var $ = require("jquery");
+  var zip = "";
+
+  $("#zip").on('click', function() {
+    zip = $('#searchField').val();
+    console.log("zip", zip);
+      if (zip.length !== 5) {
+      console.log("Please enter a 5-digit code.");
+     }
+  });
 });
