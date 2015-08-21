fa550dd660c98de84a63d61d689eff178716f31b
Deric Bunch <ddbunc2@gmail.com>
2015-08-21 12:54:40 -0500

    added auth to app

--- javascripts/dependencies.js
+++ javascripts/dependencies.js
@@ -2,4 +2,5 @@ define(function(require){
   var $ = require("jquery");
   var hbs = require("hbs");
   var bootstrap = require("bootstrap");
-})
+  var fb = require("firebase");
+});
--- javascripts/main.js
+++ javascripts/main.js
@@ -1,16 +1,39 @@
 requirejs.config({
-  baseUrl: "./javascripts",
+  baseUrl: './javascripts',
   paths: {
-    "jquery": "../lib/bower_components/jquery/dist/jquery.min",
-    "hbs": "../lib/bower_components/require-handlebars-plugin/hbs",
-    "lodash": "../lib/bower_components/lodash/lodash.min",
-    "bootstrap": "../lib/bower_components/bootstrap/dist/js/bootstrap.min"
+    'jquery': '../lib/bower_components/jquery/dist/jquery.min',
+    'hbs': '../lib/bower_components/require-handlebars-plugin/hbs',
+    'bootstrap': '../lib/bower_components/bootstrap/dist/js/bootstrap.min',
+    'firebase': '../lib/bower_components/firebase/firebase',
+    'lodash': '../lib/bower_components/lodash/lodash.min'
   },
   shim: {
-    "bootstrap": ["jquery"]  }
+    'bootstrap': ['jquery'],
+    'firebase': {
+      exports: 'Firebase'
+    }
+  }
 });
 
-requirejs(["dependencies"], 
-  function(dependencies) {
+requirejs(["dependencies", "firebase", "auth"], 
+  function(dependencies, firebase, auth) {
 
+  var ref = new Firebase("https://dld-weather.firebaseio.com/");
+  var authData = ref.getAuth();
+
+  if (authData === null) {
+    ref.authWithOAuthPopup("github", function(error, authData) {
+      if (error) {
+        console.log("Login Failed!", error);
+      } else {
+        console.log("Authenticated successfully with payload:", authData);
+        auth.setUid(authData.uid);
+       // require(["core_list"], function() {});
+      }
+    });
+  } else {
+    auth.setUid(authData.uid);
+    // require(["core_list"], function() {});
+  }
+  console.log(authData);
 });
