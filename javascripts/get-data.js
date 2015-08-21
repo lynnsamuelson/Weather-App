a867442d797a1aa3ee647850b52b037e108f9894
Lynn Samuelson <lynn.samuelson@gmail.com>
2015-08-21 13:18:49 -0500

    API call working

--- javascripts/get-data.js
+++ javascripts/get-data.js
@@ -1,7 +1,18 @@
 define(function(require) {
   var $ = require("jquery");
 
-   //$.getJSON("https://deric-trippin.firebaseio.com/trips.json", function(data) {
-  
+  //$.getJSON("https://api.openweathermap.org/data/2.5/weather?zip=94040,us&APPID=514959dd42853402865197cec43244f4", function(data) {
+  $.ajax({
+    url: "http://api.openweathermap.org/data/2.5/weather?zip=37221,us&APPID=514959dd42853402865197cec43244f4",
+    method: "GET",
+    data: JSON
+    }).done(function(data) {
+      console.log("weather", data);
+    });
+ 
 
-});
+
+
+     //$('#postWeatherTest').html("");
+  });
+
