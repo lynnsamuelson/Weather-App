b9907d662e1f840cd017867d09270ad3efc79205
Dan-Hodges <dhdgs690@gmail.com>
2015-08-21 13:47:00 -0500

    fixed alignment

--- javascripts/get-data.js
+++ javascripts/get-data.js
@@ -1,14 +1,13 @@
 define(function(require) {
   var $ = require("jquery");
 
-  
   $.ajax({
     url: "http://api.openweathermap.org/data/2.5/weather?zip=37221,us&APPID=514959dd42853402865197cec43244f4",
     method: "GET",
     data: JSON
-    }).done(function(data) {
+  }).done(function(data) {
       console.log("weather", data);
-    });
- 
   });
+ 
+});
 
