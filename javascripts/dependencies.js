e29c4c8fcf01b190accbd42b4f3d725e9e9176f2
Deric Bunch <ddbunc2@gmail.com>
2015-08-21 11:22:24 -0500

    added dependencies and main content

---
+++ javascripts/dependencies.js
@@ -0,0 +1,5 @@
+define(function(require){
+  var $ = require("jquery");
+  var hbs = require("hbs");
+  var bootstrap = require("bootstrap");
+})
---
+++ javascripts/main.js
@@ -0,0 +1,16 @@
+requirejs.config({
+  baseUrl: "./javascripts",
+  paths: {
+    "jquery": "../lib/bower_components/jquery/dist/jquery.min",
+    "hbs": "../lib/bower_components/require-handlebars-plugin/hbs",
+    "lodash": "../lib/bower_components/lodash/lodash.min",
+    "bootstrap": "../lib/bower_components/bootstrap/dist/js/bootstrap.min"
+  },
+  shim: {
+    "bootstrap": ["jquery"]  }
+});
+
+requirejs(["dependencies"], 
+  function(dependencies) {
+
+});
