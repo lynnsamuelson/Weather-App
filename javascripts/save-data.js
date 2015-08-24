c8008b6d9b991f545cf7f74bfe0e8ade639ec6d1
Deric Bunch <ddbunc2@gmail.com>
2015-08-24 09:51:32 -0500

    v1 complete

--- javascripts/save-data.js
+++ javascripts/save-data.js
@@ -5,13 +5,13 @@ define(function(require){
 
   $(document).on('click', '#saveForecast', function(weather){
     forecast = $(this).parent().text().split('\n');
-    // forecast = forecast.replace('\t', '');
+    console.log(forecast);
     var saveForecast = {
-      temp: forecast[2],
-      current: forecast[3],
-      pressure: forecast[5],
-      speed: forecast[6],
-      date: forecast[7]
+      temp: forecast[3],
+      current: forecast[4],
+      pressure: forecast[6],
+      speed: forecast[7],
+      date: forecast[2]
     };
     var fb = new Firebase("https://dld-weather.firebaseio.com/");
     fb.push({saveForecast});
--- javascripts/threeDay.js
+++ javascripts/threeDay.js
@@ -6,18 +6,33 @@ define(function(require) {
   	function kelToFah (kelvin) {
   		return Math.round((kelvin * (9/5)) - 459.67);
   	}
+
+  function timeConverter(unix){
+    var a = new Date(unix * 1000);
+    var months = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec'];
+    var year = a.getFullYear();
+    var month = months[a.getMonth()];
+    var date = a.getDate();
+    var hour = a.getHours();
+    var min = a.getMinutes();
+    var sec = a.getSeconds();
+    var time = date + ' ' + month + ' ' + year;
+    return time;
+  }
   	var deferred = Q.defer();
 
 		$.ajax({
-		  url: "http://api.openweathermap.org/data/2.5/forecast?lat=" + coordinates,
+		  url: "http://api.openweathermap.org/data/2.5/forecast/daily?lat=" + coordinates + "&cnt=07",
 		  method: "GET",
 		  data: JSON
 		}).done(function(data) {	
 			var weather = data.list;
 			console.log("weather :", weather);
 			for (var i = 0; i < weather.length; i++) {
-				weather[i].main.temp = 
-				kelToFah(weather[i].main.temp);
+				weather[i].temp.day = 
+				kelToFah(weather[i].temp.day);
+        weather[i].dt = 
+        timeConverter(weather[i].dt);
 			}
 	    deferred.resolve(weather);
 		}); 
